dbf3b5f24e3e0670417a559bdf2346773a4fd9dd
Mike Vink <mike1994vink@gmail.com>
2022-12-12 16:50:18 +0100

    golines

--- flake.nix
+++ flake.nix
@@ -17,6 +17,21 @@
           go_1_19
           gofumpt
           gotools
+          (pkgs.buildGoModule rec {
+            pname = "golines";
+            version = "0.11.0";
+
+            src = pkgs.fetchFromGitHub {
+              owner = "segmentio";
+              repo = "golines";
+              rev = "v${version}";
+              sha256 = "sha256-2K9KAg8iSubiTbujyFGN3yggrL+EDyeUCs9OOta/19A=";
+            };
+
+            vendorSha256 = "sha256-rxYuzn4ezAxaeDhxd8qdOzt+CKYIh03A9zKNdzILq18=";
+
+            nativeBuildInputs = [pkgs.installShellFiles];
+          })
         ];
       };
     });
